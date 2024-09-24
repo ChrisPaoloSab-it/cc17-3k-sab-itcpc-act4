202bb0f2bf76fa5a85b531bdaf5f6766e18264a2
ChrisPaoloSab-it <cpaocastillos@gmail.com>
2024-09-25 00:21:37 +0800

    changed code and fixed errors

--- app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.tipcalculator
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioGroup
+import android.widget.Switch
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var roundup: Switch
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,30 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val button: Button = findViewById(R.id.calculatebutton)
+        val tipResult = findViewById<TextView>(R.id.tipTotal)
+        roundup = findViewById<Switch>(R.id.roundUp)
+
+        button.setOnClickListener {
+            val tip = tipCalculator()
+            val formattedTip = String.format("%.2f", tip)
+            tipResult.text = formattedTip
+        }
+    }
+        private fun tipCalculator():Double {
+            val cost = findViewById<EditText>(R.id.initialamount).text.toString().toDoubleOrNull() ?: 0.0
+            val tipChoices = findViewById<RadioGroup>(R.id.tipChoices)
+            val tipPercentage = when (tipChoices.checkedRadioButtonId) {
+                R.id.amazing -> 0.20
+                R.id.good -> 0.18
+                R.id.ok -> 0.15
+                else -> 0.0
+            }
+            if (roundup.isChecked) {
+                return kotlin.math.ceil(cost * tipPercentage)
+            }
+            return cost * tipPercentage
+
+
     }
 }
